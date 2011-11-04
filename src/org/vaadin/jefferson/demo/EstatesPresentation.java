66f9487bb0fdb1195732101caef4ad4fc32d1747
Marlon Richert <marlon@vaadin.com>
2011-11-04 17:20:10 +0200

    redefinition of "tabs" to pass run-time type checking

--- src/org/vaadin/jefferson/demo/EstatesPresentation.java
+++ src/org/vaadin/jefferson/demo/EstatesPresentation.java
@@ -7,19 +7,20 @@ import org.vaadin.jefferson.content.UIElement;
 import com.vaadin.terminal.Sizeable;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.NativeSelect;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalSplitPanel;
 
 public class EstatesPresentation extends Presentation {
     public EstatesPresentation() {
         define(SelectionControl.class, Table.class);
+        define("tabs", NativeSelect.class);
 
         define("main", VerticalSplitPanel.class);
         define("main", method("main"));
 
         define("properties", HorizontalLayout.class);
 
-        define("tabs", TabSegment.class);
         define("root", method("root"));
         define("nav", method("bar"));
         define("estate-info", method("bar"));
